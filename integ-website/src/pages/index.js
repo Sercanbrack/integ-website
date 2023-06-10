ab58777d1efc3b329c2565168f6da8e31916697b
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2023-06-10 10:32:46 -0600

    built hamburger menu for mobile viewing

--- integ-website/src/pages/index.js
+++ integ-website/src/pages/index.js
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <NavBar className="fixed left-0 top-0 flex w-full"></NavBar>
+    <main className="min-h-screen flex-col items-center justify-between">
+      <NavBar className="fixed left-0 top-0 flex w-screen"></NavBar>
     </main>
   )
 }
