fe34750d152dbc481d52944c2f2c647b61cc842f
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2024-03-20 17:08:02 -0600

    fixing up the nav menus

--- integ-website/src/components/HamburgerMenu.js
+++ integ-website/src/components/HamburgerMenu.js
@@ -14,25 +14,25 @@ const HamburgerMenu = () => {
              className="hamburger lg:hidden cursor-pointer text-2xl p-3 justify-end"
              onClick={toggleMenu}
             >
-            <span className="hamburger-bar block w-5 h-1 bg-teal-700 mb-1"></span>
-            <span className="hamburger-bar block w-5 h-1 bg-teal-700 mb-1"></span>
-            <span className="hamburger-bar block w-5 h-1 bg-teal-700"></span>
+            <span className="hamburger-bar block w-10 h-1 bg-teal-700 mb-2"></span>
+            <span className="hamburger-bar block w-10 h-1 bg-teal-700 mb-2"></span>
+            <span className="hamburger-bar block w-10 h-1 bg-teal-700"></span>
         </div>
         <ul
             className={`nav-menu ${isOpen ? 'block' : 'hidden'} lg:flex flex-row lg:flex-row`}
         >
             <li className="nav-menu-item">
-                <Link href="about" className="nav-menu-link block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                <Link href="about" className="nav-menu-link block mt-4 lg:inline-block lg:mt-0 text-teal-200 sm:text-3xl sm:pr-2 lg:text-5xl lg:pr-4 hover:text-white mr-4">
                     About
                 </Link>
             </li>
             <li className="nav-menu-item">
-                <Link href="previous-work" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                <Link href="previous-work" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 sm:text-3xl sm:pr-2 lg:text-5xl lg:pr-4 hover:text-white mr-4">
                     Previous Work
                 </Link>
             </li>
             <li className="nav-menu-item">
-                <Link href="contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                <Link href="contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 sm:text-3xl sm:pr-2 lg:text-5xl lg:pr-4 hover:text-white mr-4">
                     Contact
                 </Link>
             </li>
